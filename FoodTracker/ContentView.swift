28c69fe0354a03eee42944038a05e3fe7b894cae
noriegab1 <ben.e.noriega@gmail.com>
2021-02-24 20:29:08 -0700

    Initial commit

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Ben Noriega on 2/24/21.
+//  Copyright © 2021 NordiGroup. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
